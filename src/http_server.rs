80676e92eebc1df48e74b1a359a18bc8a5c81144
hkreie <91433473+hkreie@users.noreply.github.com>
2021-12-01 13:06:45 -0700

    Update http_server.rs

--- src/http_server.rs
+++ src/http_server.rs
@@ -10,78 +10,109 @@ use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 use std::env;
 use std::sync::Mutex;
 
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+
+
 use crate::cli;
 use crate::db::GlobalState;
 use crate::foobar;
 
-use std::process::Command;
+use std::{thread, time};
 
 // This is using /admin/diskspace route
 #[get("/diskspace")]
 fn diskspace() -> String {
-    let output = Command::new("df")
-        .arg("-H")
-        .output()
-        .expect("Failed to execute command");
-    let s = String::from_utf8(output.stdout).expect("Found invalid UTF-8");
-    format!("{}", s)
+    let mut file = File::create("/dev/ttyACM0").unwrap();
+    file.write_all(b"diskspace\n");
+    file.flush();
+    let millis = time::Duration::from_millis(200);
+    let now = time::Instant::now();
+    thread::sleep(millis);
+    let mut f = File::open("/dev/ttyACM0").unwrap();
+    let mut contents0 = String::new();
+    let mut contents1 = String::new();
+    let mut contents2 = String::new();
+    let mut contents3 = String::new();
+    let mut contents4 = String::new();
+    let mut contents5 = String::new();
+    let mut contents6 = String::new();
+    let mut contents7 = String::new();
+    let mut contents8 = String::new();
+    let mut contents9 = String::new();
+    let mut contentsA = String::new();
+    let mut contentsB = String::new();
+    let mut contentsC = String::new();
+    let mut contentsD = String::new();
+    let mut contentsE = String::new();
+    let mut contentsF = String::new();
+    let mut f = BufReader::new(f);
+    f.read_line(&mut contents0);
+    f.read_line(&mut contents1);
+    f.read_line(&mut contents2);
+    f.read_line(&mut contents3);
+    f.read_line(&mut contents4);
+    f.read_line(&mut contents5);
+    f.read_line(&mut contents6);
+    f.read_line(&mut contents7);
+    f.read_line(&mut contents8);
+    f.read_line(&mut contents9);
+    f.read_line(&mut contentsA);
+    f.read_line(&mut contentsB);
+    f.read_line(&mut contentsC);
+    f.read_line(&mut contentsD);
+    f.read_line(&mut contentsE);
+    f.read_line(&mut contentsF);
+    format!("{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}", contents0, contents1, contents2, contents3, contents4, contents5, contents6, contents7, contents8, contents9, contentsA, contentsB, contentsC, contentsD, contentsE, contentsF)
 }
 
 // This is using /admin/cputemp route
 #[get("/cputemp")]
 fn cputemp() -> String {
-    let s = "cputemp";
-    format!("{}", s)
+    let mut file = File::create("/dev/ttyACM0").unwrap();
+    file.write_all(b"cputemp\n");
+    file.flush();
+    let millis = time::Duration::from_millis(200);
+    let now = time::Instant::now();
+    thread::sleep(millis);
+    let mut f = File::open("/dev/ttyACM0").unwrap();
+    let mut contents1 = String::new();
+    //let mut contents2 = String::new();
+    let mut f = BufReader::new(f);
+    f.read_line(&mut contents1);
+    f.read_line(&mut contents1);
+    //f.read_line(&mut contents2);
+    format!("{}", contents1)
+    //format!("{}<br />{}", contents1, contents2)
 }
 
-// This is using /admin/cputemp route
-#[get("/journalctl")]
-fn journalctl() -> String {
-    let s = "journalctl";
-    format!("{}", s)
-}
 
-
-// This is using /admin/reboot route
-#[get("/reboot")]
-fn reboot() -> String {
-    let output = Command::new("reboot")
-        .output()
-        .expect("Failed to execute command");
-    let s = String::from_utf8(output.stdout).expect("Found invalid UTF-8");
-    format!("{}", s)
-}
-//
 // This is using /admin/uptime route
 #[get("/uptime")]
 fn uptime() -> String {
-    let output = Command::new("uptime")
-        .output()
-        .expect("Failed to execute command");
-    let s = String::from_utf8(output.stdout).expect("Found invalid UTF-8");
-    format!("{}", s)
-}
-
-// This is using /admin/status route
-#[get("/performance")]
-fn performance() -> String {
-    let output = Command::new("systemd-analyze")
-        .output()
-        .expect("Failed to execute command");
-    let s = String::from_utf8(output.stdout).expect("Found invalid UTF-8");
-    format!("{}", s)
+    let mut file = File::create("/dev/ttyACM0").unwrap();
+    file.write_all(b"uptime\n");
+    file.flush();
+    let millis = time::Duration::from_millis(200);
+    let now = time::Instant::now();
+    thread::sleep(millis);
+    let mut f = File::open("/dev/ttyACM0").unwrap();
+    let mut contents1 = String::new();
+    let mut f = BufReader::new(f);
+    f.read_line(&mut contents1);
+    f.read_line(&mut contents1);
+    f.read_line(&mut contents1);
+    format!("{}", contents1)
 }
 
 
 // This is using /admin/status route
 #[get("/status")]
 fn status() -> String {
-    let output = Command::new("top")
-        .args(["-b","-n","1"])
-        .output()
-        .expect("Failed to execute command");
-    let s = String::from_utf8(output.stdout).expect("Found invalid UTF-8");
-    format!("{}", s)
+    format!("Rust says your status is super")
 }
 
 // This is using /hello route
@@ -116,7 +147,7 @@ pub fn build_app(opt: cli::Opt) -> Rocket {
     rocket::ignite()
         .manage(global_state)
         .mount("/hello", routes![hello, message])
-        .mount("/admin", routes![status, diskspace, uptime, cputemp, reboot, performance])
+        .mount("/admin", routes![status, diskspace, uptime, cputemp])
         // routes for which we have the #[openapi] attribute specified
         .mount("/", openapi_routes)
         // http:<hostname>:<port>/api presents a web page
