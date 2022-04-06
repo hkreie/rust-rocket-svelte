ed6b59d90183d5a26946504caa47355278cc79a7
Hunter Kreie <hkreie@asu.edu>
2022-04-05 22:15:32 -0700

    Updated Settings page to call back end settings route

--- client/Admin/Settings.svelte
+++ client/Admin/Settings.svelte
@@ -1,4 +1,22 @@
-<h1>Settings</h1>
-<p>
-    Device settings
-</p>
+<script>
+    import { request } from "../Server/Request.svelte";
+  
+    // import Request from "../Server/Request.svelte";
+    let url = "/admin/settings";
+    let promise = request(url);
+    function handleClick() {
+      promise = request(url);
+    }
+</script>
+  
+<main>
+    <h1>Settings</h1>
+    {#await promise}
+      <p>...Retrieving status from Rust (with a 1 second sleep)</p>
+    {:then text}
+      <p>Message from Rust: <pre>{text}</pre>
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+    <button on:click={handleClick}>Refresh</button>
+</main>
